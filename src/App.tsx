2be2f6d2f7494d2400f2b8f1773642ab0e2798d1
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-17 20:43:20 +0100

    reactj

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const App: React.FC<{}> = () => {
+  const [value, setValue] = useState(true)
+  const [val, setval] = useState(true)
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Titre 
+      titre = "Liste de Courses"
+      />
+      
+      <Formulaire value={"Pomme"} checkboxValue={value} setValue={setValue}/>
+      <Formulaire value={"Poire"} checkboxValue={val} setValue={setval}/>
+      <Footer Pomme={value} Poire={val}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+const Titre = (props) => {
+  return <div>{props.titre}{props.nombre}</div>
+}
+
+const Formulaire = (props) => {
+
+  
+  return <div>
+    <input
+    onChange={(e) => props.setValue(e.target.checked)}
+    checked={props.checkboxValue}
+    type='checkbox'/>
+     {props.value} 
+    </div>
+}
+
+const Footer = (props) => {
+  const displaylist= (checkpomme,checkpoire) => {
+    const array=[]
+    if(!checkpoire){
+      array.push(" Poire")
+    }
+    if(!checkpomme){
+      array.push(" Pomme")
+    }
+    return array
+  }
+  
+  return <div>
+    Elements manquants:
+    {displaylist(props.Pomme,props.Poire)}
+  </div>
+}
